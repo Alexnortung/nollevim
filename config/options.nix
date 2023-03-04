b354d729401ef135c3a4d7ece498e6aef0500a07
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-03-04 02:24:50 +0100

    Fixed options

--- config/options.nix
+++ config/options.nix
@@ -1,18 +1,20 @@
 {
-  options = {
-    number = true; # sets numbers in the side
-    relativenumber = true; # makes side numbers relative to the cursor
-    expandtab = true; # nicer default tabs
-    clipboard = "unnamedplus"; # use system clipboard
-    mouse = "a"; # make neovim usable with mouse
-    smartcase = true; # "smart" search
-    ignorecase = true;
-    splitbelow = true;
-    splitright = true;
-    shiftwidth = 4;
-    tabstop = 4;
-    cursorline = true;
-    smartindent = 1;
-    scrolloff = 4; # keeps lines above and below
+  config = {
+    options = {
+      number = true; # sets numbers in the side
+      relativenumber = true; # makes side numbers relative to the cursor
+      expandtab = true; # nicer default tabs
+      clipboard = "unnamedplus"; # use system clipboard
+      mouse = "a"; # make neovim usable with mouse
+      smartcase = true; # "smart" search
+      ignorecase = true;
+      splitbelow = true;
+      splitright = true;
+      shiftwidth = 4;
+      tabstop = 4;
+      cursorline = true;
+      smartindent = true;
+      scrolloff = 4; # keeps lines above and below
+    };
   };
 }
