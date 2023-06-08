257c0ce44f1002d4b6ac6c6d0fb34de048d1c4e8
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-06-08 12:50:31 +0200

    chore: formatting

--- config/completion.nix
+++ config/completion.nix
@@ -7,16 +7,18 @@
     mapping = {
       "<C-b>" = ''cmp.mapping.scroll_docs(-4)'';
       "<C-f>" = ''cmp.mapping.scroll_docs(4)'';
-      "<C-Space>" = ''        cmp.mapping.complete({
-                    config = {
-                      sources = {
-                        { name = "nvim_lsp" },
-                        { name = "luasnip" },
-                        { name = "path" },
-                        { name = "buffer" },
-                      }
-                    }
-                  })'';
+      "<C-Space>" = ''
+        cmp.mapping.complete({
+          config = {
+            sources = {
+              { name = "nvim_lsp" },
+              { name = "luasnip" },
+              { name = "path" },
+              { name = "buffer" },
+            }
+          }
+        })
+      '';
       "<C-e>" = "cmp.mapping.abort()";
       "<CR>" = "cmp.mapping.confirm({ select = true })";
       "<Tab>" = {
