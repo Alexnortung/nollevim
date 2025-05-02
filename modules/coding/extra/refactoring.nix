f4735ded4735b4cc78486a7fa3edaf2a831e222c
Alexander Nortung <alexander.nortung@oakdigital.dk>
2025-05-02 08:50:15 +0200

    feat(refactoring): Added enable keymaps option

--- modules/coding/extra/refactoring.nix
+++ modules/coding/extra/refactoring.nix
@@ -2,10 +2,16 @@
 
 let
   cfg = config.nollevim.coding.extra.refactoring;
+  mkRaw = lib.nixvim.mkRaw;
 in
 {
   options.nollevim.coding.extra.refactoring = {
     enable = lib.mkEnableOption "refactoring";
+    enableKeymaps = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable refactoring keymaps";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -13,5 +19,23 @@ in
       enable = true;
       enableTelescope = lib.mkDefault true;
     };
+
+    # keymaps =
+    #   let
+    #     leader = "<leader>cc";
+    #   in
+    #   lib.mkIf cfg.enableKeymaps [
+    #     {
+    #       mode = [
+    #         "n"
+    #         "x"
+    #       ];
+    #       key = "${leader}c";
+    #       # action = "<cmd>lua require('refactoring').select_refactor()<cr>";
+    #       action = mkRaw ''
+    #         require('refactoring').select_refactor()
+    #       '';
+    #     }
+    #   ];
   };
 }
